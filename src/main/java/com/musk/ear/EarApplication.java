a5592587553abd37ab95992699f638b102123793
安河桥北 <musk@anheqiaobeidemac-mini.local>
2024-12-22 18:35:40 +0800

    2024-12-22

---
+++ src/main/java/com/musk/ear/EarApplication.java
@@ -0,0 +1,13 @@
+package com.musk.ear;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EarApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/musk/ear/EarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.musk.ear;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
